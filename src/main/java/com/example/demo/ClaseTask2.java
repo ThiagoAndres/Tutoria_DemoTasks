8fdabcc314f43aab98d3fda9aa0fdf0e74c714c0
ThiagoAndres <andreswcc11@gmail.com>
2022-08-16 21:07:49 -0500

    Se agrega constructor a ClaseTask2 con rama1

---
+++ src/main/java/com/example/demo/ClaseTask2.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+public class ClaseTask2 {
+    String nombre;
+    int annio;
+
+ public  ClaseTask2(String nombre, int annio){
+     this.nombre=nombre;
+     this.annio=annio;
+ }
+
+}
